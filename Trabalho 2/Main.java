8b09d77b5202ef0ac43b709e5551a5cec7fe604a
castelojb <34077567+castelojb@users.noreply.github.com>
2018-03-27 08:48:36 -0300

    Add files via upload
    Trabalho 2

---
+++ Trabalho 2/Main.java
@@ -0,0 +1,7 @@
+class Main{
+	public static void main(String[] args) {
+		float x[] = {1, 1};
+		float y[] = {2, 2};
+		Quadrado q = new Quadrado(x, y);
+	}
+}
---
+++ Trabalho 2/Quadrado.java
@@ -0,0 +1,14 @@
+public class Quadrado{
+	String forma;
+	float X[];
+	float Y[];
+	int cor;
+	Quadrado(float x[], float y[]) {
+		this.X =new float[4];
+		this.X=x;
+		this.Y =new float[4];
+		this.Y=y;
+
+	}
+
+}
