3d13d18a29ab1e934ae4a8cf2bf819c77fd5db72
Hariamy Vasconcelos <hariamymv@hotmail.com>
2018-04-08 14:34:00 -0300

    Questão 5 respondida
    Alterações em algumas partes da modelagem do problema.

--- Trabalho 04/Teste.java
+++ Trabalho 04/Teste.java
@@ -1,160 +1,3 @@
-interface ContaXPTO{
-
-    double consultarSaldo();
-
-    double creditar(double i);
-
-    double debitar(double i);
-
-    String toString();
-
-}
-class ContaXPTOBasic implements ContaXPTO{
-    double saldo;
-    int id;
-
-    public ContaXPTOBasic(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "ContaXPTOBasic{" +
-                "saldo=" + saldo +
-                ", Id=" + id +
-                '}';
-    }
-
-    @Override
-    public double consultarSaldo() {
-        return this.saldo;
-    }
-
-    @Override
-    public double creditar(double i) {
-        this.saldo += i;
-        return saldo;
-    }
-
-    @Override
-    public double debitar(double i) {
-        this.saldo -= i;
-        return saldo;
-    }
-}
-
-class ContaXPTOPlus implements ContaXPTO{
-    double saldo;
-    int id;
-
-    public ContaXPTOPlus(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "ContaXPTOPlus{" +
-                "saldo=" + saldo +
-                ", Id=" + id +
-                '}';
-    }
-
-    @Override
-    public double consultarSaldo() {
-        return this.saldo;
-    }
-
-    @Override
-    public double creditar(double i) {
-        double div = 0.5 /100;
-        double multi = div * i;
-        this.saldo = saldo + i + multi;
-        return saldo;
-    }
-
-    @Override
-    public double debitar(double i) {
-        this.saldo -= i;
-        return saldo;
-    }
-}
-
-class ContaXPTOExtreme implements ContaXPTO{
-    double saldo;
-    int id;
-
-    public ContaXPTOExtreme(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "ContaXPTOExtreme{" +
-                "saldo=" + saldo +
-                ", Id=" + id +
-                '}';
-    }
-
-    @Override
-    public double consultarSaldo() {
-        return this.saldo;
-    }
-
-    @Override
-    public double creditar(double i) {
-        double div = 0.2 /100;
-        double multi = div * i;
-        this.saldo = saldo + i + multi;
-        return saldo;
-    }
-
-    @Override
-    public double debitar(double i) {
-        double div = 0.2 /100;
-        double multi = div * i;
-        this.saldo = (saldo - i) + multi;
-        return saldo;
-    }
-}
-
-
-
-class Sistema  {
-
-    ContaXPTO[] contas;
-
-    public Sistema() {
-        this.contas = new ContaXPTO[4];
-    }
-
-    public void criarContaXPTOBasic(int i){
-        this.contas[i] = new ContaXPTOBasic(i);
-    }
-
-    public void criarContaXPTOPlus(int i){
-        this.contas[i] = new ContaXPTOPlus(i);
-    }
-
-    public void criarContaXPTOExtreme(int i){
-        this.contas[i] = new ContaXPTOExtreme(i);
-    }
-
-    public void creditar(int i,double c){
-        this.contas[i].creditar(c);
-    }
-
-    public void debitar(int i,double c){
-        this.contas[i].debitar(c);
-    }
-
-    public double consultarSaldo(int i){
-        return this.contas[i].consultarSaldo();
-    }
-
-
-
-}
-
 public class Teste{
     public static void main(String[] args) {
         Sistema s = new Sistema();
@@ -162,13 +5,27 @@ public class Teste{
         s.criarContaXPTOPlus(2);
         s.criarContaXPTOExtreme(3);
 
-        for (int i = 1; i <=3 ; i++) s.creditar(i,1000);
+        for (int i = 1; i <=3 ; i++) s.creditar(i, 1000);//Coerção, pois ao creditar 1000 o compilador faz a conversão de int para double
 
-        for (int i = 1; i <=3 ; i++) s.debitar(i,10);
+        for (int i = 1; i <=3 ; i++) s.debitar(i, 10);//Coerção, pois ao creditar 10 o compilador faz a conversão de int para double
 
         System.out.println(s.consultarSaldo(1));
         System.out.println(s.consultarSaldo(2));
         System.out.println(s.consultarSaldo(3));
 
+		if (s.consultarSaldo(1) == 990) {
+            System.out.println("bom1");
+        }
+        if (s.consultarSaldo(2) == 995) {
+            System.out.println("bom2");
+        }   
+        if (s.consultarSaldo(3) == 992.02) {
+            System.out.println("bom3");
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(s.contas[i]);
+            
+        }
     }
 }
