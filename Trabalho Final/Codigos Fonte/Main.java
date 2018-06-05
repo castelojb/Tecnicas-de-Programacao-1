4238e1c263d3937ff802e1df99dc8955fb154a9b
Hariamy Vasconcelos <hariamymv@hotmail.com>
2018-06-04 21:26:35 -0300

    Add files via upload

--- Trabalho Final/Codigos Fonte/Main.java
+++ Trabalho Final/Codigos Fonte/Main.java
@@ -1,8 +1,9 @@
-import view.Janela;
-import view.PainelInicial;
+import view.janela.Janela;
+import view.janela.PainelInicial;
 
 public class Main{
     public static void main(String args[]) {
+        System.setProperty("file.encoding", "UTF-8");
         Janela janela = new Janela();
         janela.conteudoJanela(new PainelInicial(janela));
         janela.exibirJanela();
